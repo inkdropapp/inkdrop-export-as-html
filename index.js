c88b507b9f7b29ac35de425ffdcb73b5a8619343
Takuya Matsuyama <nora@odoruinu.net>
2016-09-02 11:59:09 +0900

    Support images

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use babel'
 import { remote } from 'electron'
 import path from 'path'
 import fs from 'fs'
@@ -10,16 +11,12 @@ module.exports = {
     })
   },
 
-  exportAsHTML () {
+  async exportAsHTML () {
     const templateFilePath = path.join(__dirname, 'template.html')
     const templateHtml = fs.readFileSync(templateFilePath, 'utf-8')
     const { MDEPreview } = inkdrop.components.classes
-    const { document } = inkdrop.flux.getStore('note').getState()
+    const { document } = inkdrop.flux.getStore('editor').getState()
     if (document) {
-      const markdown = `# ${document.title}\n${document.body}`
-      const htmlBody = MDEPreview.renderer.render(markdown)
-      const outputHtml = templateHtml.replace('{%body%}', htmlBody)
-
       const pathToSave = dialog.showSaveDialog({
         title: 'Save HTML file',
         defaultPath: `${document.title}.html`,
@@ -28,6 +25,11 @@ module.exports = {
           { name: 'All Files', extensions: [ '*' ] }
         ]
       })
+
+      let markdown = `# ${document.title}\n${document.body}`
+      markdown = await this.replaceImages(markdown, path.dirname(pathToSave))
+      const htmlBody = MDEPreview.renderer.render(markdown)
+      const outputHtml = templateHtml.replace('{%body%}', htmlBody)
 
       if (pathToSave) {
         try {
@@ -38,6 +40,29 @@ module.exports = {
       }
     } else {
       inkdrop.notifications.addError('No note opened', { detail: 'Please open a note to export as HTML', dismissable: true })
+    }
+  },
+
+  async replaceImages (body, dirToSave) {
+    // find attachments
+    const uris = body.match(/inkdrop:\/\/file:[^\) ]*/g) || []
+    for (let i = 0; i < uris.length; ++i) {
+      const uri = uris[i]
+      const imagePath = await this.exportImage(uri, dirToSave)
+      if (imagePath) {
+        body = body.replace(uri, imagePath)
+      }
+    }
+    return body
+  },
+
+  async exportImage (uri, dirToSave) {
+    try {
+      const file = await inkdrop.models.File.getDocumentFromUri(uri)
+      return file.saveFileSync(dirToSave)
+    } catch (e) {
+      console.error('Failed to export image file:', e)
+      return false
     }
   },
 
